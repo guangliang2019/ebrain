870d099247540a7bf711ff5fd1d0e24ec0cecb88
gl2020 <2606298246@qq.com>
2020-10-18 21:41:20 +0800

    main2

---
+++ src/components/LogoZone.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const style = {
+    root: {
+        background: '#FFFFFF',
+        width: '200px',
+        height: '80px',
+        borderRight: '1px solid #D9D9D9',
+    },
+
+}
+
+class LogoZone extends React.Component {
+    render() {
+        return (
+            <div style={style.root}>
+            </div >
+        )
+    }
+}
+
+export default LogoZone
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import LogoZone from './LogoZone.jsx'
 
 const style = {
     root: {
         background: '#FFFFFF',
         width: '100vw',
         height: '80px',
+        display: 'flex',
+        flexDirection: 'row',
     },
 
 }
@@ -13,7 +16,7 @@ class NavBar extends React.Component {
     render() {
         return (
             <div style={style.root}>
-
+                <LogoZone />
             </div>
         )
     }
